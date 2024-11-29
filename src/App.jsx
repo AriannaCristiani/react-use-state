25fb8f529ea25bc0a4e4268da153f8de79820def
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-11-29 17:27:13 +0100

    implementazione dei bottoni con i titoli dell'array

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from './components/Header/Header.jsx'
+import Main from './components/Main/Main.jsx'
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Header></Header>
+      <Main></Main>
 
     </>
   )
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,20 @@
 import { languages } from '../../languages.js'
+import Button from '../Button/Button.jsx'
 
 
 export default function Main() {
+
+    return (
+        <div className='container'>
+            <ul className='list'>
+                {languages.map((language) => (
+                    <li key={language.id}>
+                        <Button title={language.title} />
+                    </li>
+                ))}
+
+            </ul>
+        </div>
+    )
 
 }
