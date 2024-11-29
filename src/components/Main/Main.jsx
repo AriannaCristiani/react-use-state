1a0823dcc3b3f4ef2dc7eae78b93b5a503c14f91
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-11-29 17:43:20 +0100

    creazione dinamica di tutte le card con titolo e descrizione

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,20 +1,30 @@
 import { languages } from '../../languages.js'
 import Button from '../Button/Button.jsx'
+import Card from '../Card/Card.jsx'
 
 
 export default function Main() {
 
     return (
-        <div className='container'>
-            <ul className='list'>
-                {languages.map((language) => (
-                    <li key={language.id}>
-                        <Button title={language.title} />
-                    </li>
-                ))}
+        <section>
+            <div className='container'>
+                <ul className='list'>
+                    {languages.map((language) => (
+                        <li key={language.id}>
+                            <Button title={language.title} />
+                        </li>
+                    ))}
 
-            </ul>
-        </div>
+                </ul>
+            </div>
+            <div className='container'>
+                {languages.map((language) => (
+                    <div key={language.id} className='cards'>
+                        <Card title={language.title} description={language.description} />
+                    </div>
+                ))}
+            </div>
+        </section>
     )
 
 }
